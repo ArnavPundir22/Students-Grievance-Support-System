e0115f9e87d137b3935a68c7f473c3360e9156fa
Arnav Pundir <arnavp128@gmail.com>
2025-02-08 16:05:27 +0530

    Add files via upload

--- Students Grievance Support System/server.js
+++ Students Grievance Support System/server.js
@@ -113,3 +113,5 @@ app.get('/api/complaints', authenticateToken, async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
